27388ddc46de595110a981b576209b2bbadd4b5a
Jim McMahon <jmcmahon@sagencelearning.com>
2017-04-08 18:11:31 -0400

    ruby_blocks/issues/1 commit files from AWS CodeCommit

---
+++ BlockBasics/block_basics.rb
@@ -0,0 +1,14 @@
+5.times do |n|
+  puts "#{n} situp"
+  puts "#{n} pushup"
+  puts "#{n} chinup"
+end
+
+# Remember that block parameters sit between vertical bars at the start of the block.
+
+
+1.upto(5) do |n|
+  puts "#{n} situp"
+  puts "#{n} pushup"
+  puts "#{n} chinup"
+end
