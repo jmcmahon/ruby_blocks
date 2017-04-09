c7e18500f948d70f29c582b5d5d329e05cabfcfc
Jim McMahon <jmcmahon@sagencelearning.com>
2017-04-08 20:04:59 -0400

    ruby_blocks/issues/3 add comments to gotchas

--- BlockBasics/block_gotchas.rb
+++ BlockBasics/block_gotchas.rb
@@ -2,10 +2,10 @@ number = 100
 temp = Math::PI / 4
 
 # this temp is not the same as that defined outside of the block
-3.times do |number; temp|
-  name = "Moe"
+3.times do |number; temp| # block parameters like number are not shared with variables with same name outside of block
+  name = "Moe"   # name is local to the block; not accessible outside this block
   temp = 98.6 # we're not changing the variable defined outside block
   puts "#{number} Howdy, #{name} is #{temp}!"
 end
 
-puts "#{temp}"
+puts "#{temp}"  # will match temp from line 2
